95d608b049582abb7eeb1a8aa6036493c42bb856
domheedek <simbaa4@hotmail.com>
2020-12-17 17:57:28 +0700

    last

--- filter.py
+++ filter.py
@@ -1,10 +1,23 @@
-import re
-import pandas as pd 
-i = 'simple'
-simple = ['Iphone 12','Iphone 12 Pro','Iphone 12 Pro Max']
+# import re
+# import pandas as pd 
+# simple = ['Iphone 12','Iphone 12 Pro','Iphone 12 Pro Max']
 
 # for i in simple:
+#     j = i.split()
+#     print(type(j))
+#     print(j)
+    
+list1 = ['Perl', 'PHP', 'Java', 'ASP']
+list2 = ['JavaScript is client-side scripting language',
+         'PHP is a server-side scripting language',
+         'Java is a programming language',
+         'Bash is a scripting language']
 
+# Filter the second list based on first list
+filter_data = [x for x in list2 if
+              all(y not in x for y in list1)]
 
-print(simple.split())
-
+# Print list data before filter and after filter
+print("The content of the first list:", list1)
+print("The content of the second list:", list2)
+print("The content of the second list after filter:", filter_data)
--- lazada.py
+++ lazada.py
@@ -6,7 +6,7 @@ import pandas as pd
 # Enter the file directory of the Chromedriver
 webdriver_path = '/Users/PC_COM1/chromedriver'
 Lazada_url = 'https://www.lazada.co.th'
-search_item = 'iphone 12'  # Choose this because I often search for coffee!
+search_item = 'iphone 12'  # Choose this because I often search for Pussy!
 
 # Select custom Chrome options
 options = webdriver.ChromeOptions()
